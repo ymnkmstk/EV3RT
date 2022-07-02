a10d4efe902fa6b62409cb897a34f81c5e4e7546
shojiro <shojiro4150@yahoo.co.jp>
2022-07-02 21:29:35 +0900

    test01

--- sample_cpp5/LineTracer/LineTracer.c
+++ sample_cpp5/LineTracer/LineTracer.c
@@ -7,10 +7,11 @@ static int16_t steering_amount_calculation(void);
 static void motor_drive_control(int16_t);
 
 /* ライントレースタスク(100msec周期で関数コールされる) */
-void tracer_task(intptr_t unused) {
+void tracer_task(intptr_t unused)
+{
 
     int16_t steering_amount; /* ステアリング操舵量の計算 */
-    
+
     /* ステアリング操舵量の計算 */
     steering_amount = steering_amount_calculation();
 
@@ -22,12 +23,13 @@ void tracer_task(intptr_t unused) {
 }
 
 /* ステアリング操舵量の計算 */
-static int16_t steering_amount_calculation(void){
+static int16_t steering_amount_calculation(void)
+{
 
-    uint16_t  target_brightness; /* 目標輝度値 */
-    float32_t diff_brightness;   /* 目標輝度との差分値 */
-    int16_t   steering_amount;   /* ステアリング操舵量 */
-    rgb_raw_t rgb_val;           /* カラーセンサ取得値 */
+    uint16_t target_brightness; /* 目標輝度値 */
+    float32_t diff_brightness;  /* 目標輝度との差分値 */
+    int16_t steering_amount;    /* ステアリング操舵量 */
+    rgb_raw_t rgb_val;          /* カラーセンサ取得値 */
 
     /* 目標輝度値の計算 */
     target_brightness = (WHITE_BRIGHTNESS - BLACK_BRIGHTNESS) / 2;
@@ -45,13 +47,17 @@ static int16_t steering_amount_calculation(void){
 }
 
 /* 走行モータ制御 */
-static void motor_drive_control(int16_t steering_amount){
+static void motor_drive_control(int16_t steering_amount)
+{
 
     int left_motor_power, right_motor_power; /*左右モータ設定パワー*/
 
     /* 左右モータ駆動パワーの計算(走行エッジを右にする場合はRIGHT_EDGEに書き換えること) */
-    left_motor_power  = (int)(BASE_SPEED + (steering_amount * LEFT_EDGE));
-    right_motor_power = (int)(BASE_SPEED - (steering_amount * LEFT_EDGE));
+    /*    left_motor_power  = (int)(BASE_SPEED + (steering_amount * LEFT_EDGE));
+        right_motor_power = (int)(BASE_SPEED - (steering_amount * LEFT_EDGE));
+    */
+    left_motor_power = (int)(100);
+    right_motor_power = (int)(100);
 
     /* 左右モータ駆動パワーの設定 */
     ev3_motor_set_power(left_motor, left_motor_power);
