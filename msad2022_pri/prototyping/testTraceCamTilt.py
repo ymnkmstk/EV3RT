b0a4bcd6a22e80c8b579afa23b96741293cd270a
Wataru Taniguchi <wataru@taniguchifamily.com>
2022-07-20 06:59:49 +0900

    testTraceCamTilt.py updated

--- msad2022_pri/prototyping/testTraceCamTilt.py
+++ msad2022_pri/prototyping/testTraceCamTilt.py
@@ -5,14 +5,18 @@ import math
 import numpy as np
 import time
 
+# constants
+FRAME_WIDTH = 640
+FRAME_HEIGHT = 480
+
 # callback function for trackbars
 def nothing(x):
     pass
 
 # prepare the camera
 cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH,640)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT,480)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH,FRAME_WIDTH)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT,FRAME_HEIGHT)
 cap.set(cv2.CAP_PROP_FPS,90)
 
 # create trackbars
@@ -36,13 +40,13 @@ while True:
     v_max = cv2.getTrackbarPos("V_max", "testTrace1")
     edge  = cv2.getTrackbarPos("Edge",  "testTrace1")
 
-    dx = 320
+    dx = int(FRAME_WIDTH/2)
 
     time.sleep(0.01)
 
     ret, img = cap.read()
     # crop the nearest/bottom part of the image
-    img_bgr = img[240:480, 0:640]
+    img_bgr = img[int(FRAME_HEIGHT/2):FRAME_HEIGHT, 0:FRAME_WIDTH]
     # reduce the noise
     img_med = cv2.medianBlur(img_bgr, 5)
     # convert the image from BGR to HSV
@@ -57,11 +61,11 @@ while True:
     num_labels, img_labeled, stats, centroids = cv2.connectedComponentsWithStats(img_inv)
 
     # ignore the largest black part (the first label)
-    num_labels = num_labels - 1
     stats = np.delete(stats, 0, 0)
     centroids = np.delete(centroids, 0, 0)
+    num_labels = num_labels - 1
 
-    # prepare an empty matrix
+    # prepare empty matrices
     img_lbl = np.zeros_like(img_bin)
     img_edge_cvt = np.zeros_like(img_bgr)
     
@@ -76,10 +80,9 @@ while True:
         s = stats[index][4]
         mx = int(centroids[index][0])
         my = int(centroids[index][1])
-        dx = 320 - mx
-        # draw a circle on the centroid
+        # draw a circle at the centroid on the target image
         cv2.circle(img_tgt, (mx, my), 20, (179,0,255), -1)
-        # paint the largest component in the empty matrix
+        # copy the largest component in the empty matrix
         img_lbl[img_labeled == index+1, ] = 255
         # detect edges
         img_edge = cv2.Canny(img_lbl, 255, 255)
@@ -88,26 +91,30 @@ while True:
         # find contours
         contours, hierarchy = cv2.findContours(img_edge, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         if len(contours) >= 2:
+            # derive an approximated line for either edge by regression
             vx,vy,x0,y0 = cv2.fitLine(contours[edge], cv2.DIST_L2, 0, 0.01, 0.01)
-            m = 640
+            m = FRAME_WIDTH
             x1 = int(x0 - m*vx)
             y1 = int(y0 - m*vy)
             x2 = int(x0 + m*vx)
             y2 = int(y0 + m*vy)
             retval, p1, p2 = cv2.clipLine((0,0,img_tgt.shape[1],img_tgt.shape[0]), (x1,y1), (x2,y2))
+            # draw the line
+            #print(mx, p1, p2)
             cv2.line(img_tgt, p1, p2, (179,0,255), 10)
-            # normalize dx from -1000 to 100
-            ndx = int(dx*100/320)
-            # exaggerate the tilt
-            a = -15 # constant
+            # adjust the centroid by the line tilt
             if vy != 0:
-                tilt = a*vx*vx/vy/vy
-            else:
-                tilt = 0 # cannot determine the way to go
-            print(f"ndx = {ndx}, tilt = {tilt}")
-    
+                mx = mx - p1[0] + p2[0]
+            # indicate the adjusted centroid by the vertical line
+            cv2.line(img_tgt, (mx, 0), (mx, int(FRAME_HEIGHT/2)), (179,0,255), 10)
+            dx = int(FRAME_WIDTH/2) - mx
+            #print(f"dx = {dx}")
+
+    # concatinate the images - original + edge + target
     img_v = cv2.vconcat([img,img_edge_cvt,img_tgt])
+    # shrink the concatinated image to avoid delay in transmission
     img_comm = cv2.resize(img_v, (int(img_v.shape[1]/4), int(img_v.shape[0]/4)))
+    # transmit and display the image
     cv2.imshow("testTrace2", img_comm)
 
     c = cv2.waitKey(1) # show the window
